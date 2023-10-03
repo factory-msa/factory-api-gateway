dff2eea8eba3e6eef6175a0e8eea10e10fbd34d1
juhyun <zzang9ha@naver.com>
2023-10-04 00:34:32 +0900

    initial commit

---
+++ src/main/kotlin/com/factorygateway/FactoryGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.factorygateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FactoryGatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<FactoryGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/factorygateway/FactoryGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.factorygateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FactoryGatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
