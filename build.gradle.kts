3aae721e9174a762a256c863ae1d43b99b3b60c8
juhyun <zzang9ha@naver.com>
2023-11-22 23:02:56 +0900

    chore: add spring-cloud-config, default-filters

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ extra["springCloudVersion"] = "2021.0.8"
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
